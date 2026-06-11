3fe6d27f258e0d08ae2757c0f5d6ea6612b3798f
bila cintaaaaa <nabilaislamicinta@gmail.com>
2026-06-11 14:57:49 +0700

    [Tahap 3] Implementasi abstract class Tiket dengan properti terenkapsulasi dan abstract method

---
+++ latihan_pbo/tiket.php
@@ -0,0 +1,102 @@
+<?php
+require_once 'Koneksi.php';
+
+// Tahap 3: Implementasi Abstraksi (Abstraction)
+abstract class Tiket {
+    protected $id_tiket;
+    protected $nama_film;
+    protected $jadwal_tayang;
+    protected $jumlah_kursi;
+    protected $hargaDasarTiket;
+
+    public function __construct($id, $film, $jadwal, $kursi, $hargaDasar) {
+        $this->id_tiket = $id;
+        $this->nama_film = $film;
+        $this->jadwal_tayang = $jadwal;
+        $this->jumlah_kursi = $kursi;
+        $this->hargaDasarTiket = $hargaDasar;
+    }
+
+    abstract public function hitungTotalHarga();
+    abstract public function tampilkanInfoFasilitas();
+
+    public function getIdTiket() { return $this->id_tiket; }
+    public function getNamaFilm() { return $this->nama_film; }
+    public function getJadwalTayang() { return $this->jadwal_tayang; }
+    public function getJumlahKursi() { return $this->jumlah_kursi; }
+    public function getHargaDasar() { return $this->hargaDasarTiket; }
+}
+
+// Tahap 4: Implementasi Pewarisan (Inheritance)
+class TiketRegular extends Tiket {
+    private $tipeAudio;
+    private $lokasiBaris;
+
+    public function __construct($id, $film, $jadwal, $kursi, $hargaDasar, $audio, $baris) {
+        parent::__construct($id, $film, $jadwal, $kursi, $hargaDasar);
+        $this->tipeAudio = $audio;
+        $this->lokasiBaris = $baris;
+    }
+
+    public function hitungTotalHarga() {
+        return $this->hargaDasarTiket * $this->jumlah_kursi;
+    }
+
+    public function tampilkanInfoFasilitas() {
+        return "Audio: " . $this->tipeAudio . " | Baris: " . $this->lokasiBaris;
+    }
+}
+
+class TiketIMAX extends Tiket {
+    private $kacamata3dId;
+    private $efekGerakFitur;
+
+    public function __construct($id, $film, $jadwal, $kursi, $hargaDasar, $kacamataId, $efekGerak) {
+        parent::__construct($id, $film, $jadwal, $kursi, $hargaDasar);
+        // PERBAIKAN: Menggunakan variabel parameter $kacamataId yang benar
+        $this->kacamata3dId = $kacamataId ?? 'Tidak Ada';
+        $this->efekGerakFitur = $efekGerak ?? 'Standar';
+    }
+
+    public function hitungTotalHarga() {
+        $biayaTambahan = 25000 * $this->jumlah_kursi;
+        return ($this->hargaDasarTiket * $this->jumlah_kursi) + $biayaTambahan;
+    }
+
+    public function tampilkanInfoFasilitas() {
+        return "Kacamata ID: " . $this->kacamata3dId . " | Efek: " . $this->efekGerakFitur;
+    }
+}
+
+class TiketVelvet extends Tiket {
+    private $bantalSelimutPack;
+    private $layananButler;
+
+    public function __construct($id, $film, $jadwal, $kursi, $hargaDasar, $bantalSelimut, $butler) {
+        parent::__construct($id, $film, $jadwal, $kursi, $hargaDasar);
+        $this->bantalSelimutPack = $bantalSelimut == 1 ? "Termasuk" : "Tidak";
+        $this->layananButler = $butler == 1 ? "Aktif" : "Tidak";
+    }
+
+    public function hitungTotalHarga() {
+        $biayaPremium = 100000 * $this->jumlah_kursi;
+        return ($this->hargaDasarTiket * $this->jumlah_kursi) + $biayaPremium;
+    }
+
+    public function tampilkanInfoFasilitas() {
+        return "Bantal/Selimut: " . $this->bantalSelimutPack . " | Butler Service: " . $this->layananButler;
+    }
+}
+
+// Class Manager untuk fetching data (Dependency Injection)
+class DataTiket {
+    private $db;
+    public function __construct($dbConnection) {
+        $this->db = $dbConnection;
+    }
+    public function ambilSemuaTiket() {
+        $query = "SELECT * FROM tabel_tiket";
+        return $this->db->query($query);
+    }
+}
+?>
